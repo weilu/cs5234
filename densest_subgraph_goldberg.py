755e4542affa49355ce1dd95e049732cc432f675
Wei Lu <luwei.here@gmail.com>
2018-10-30 22:08:39 +0800

    Complete goldberg algo implementation

--- densest_subgraph_goldberg.py
+++ densest_subgraph_goldberg.py
@@ -25,7 +25,6 @@ def undirected_to_flow_network(graph):
     vertex_labels[t] = 't'
 
     m = len(graph) # original edges
-    g = m/2
     for v in network.vertices():
         if v == s or v == t:
             continue
@@ -34,22 +33,65 @@ def undirected_to_flow_network(graph):
         sv = network.add_edge(s, v, add_missing=False)
         cap[sv] = m
 
-        # add vertex -> sink edges, set all edge capacity to m + 2g - deg(v)
+        # add vertex -> sink edges, set edge capacity later
         vt = network.add_edge(v, t, add_missing=False)
-        cap[vt] = m + 2 * g - deg[v]
 
     # sanity check
     v = set(list(itertools.chain(*graph)))
     assert(network.num_vertices() == len(v) + 2)
     assert(network.num_edges() == 2*m + 2*len(v))
 
-    return network
+    return network, s, t
+
+
+def update_sink_edge_weights(network, s, t, m, g):
+    deg = network.vertex_properties['degrees']
+    cap = network.edge_properties["cap"]
+    for e in t.in_edges():
+        # set all sink edge capacity to m + 2g - deg(v)
+        cap[e] = m + 2 * g - deg[e.source()]
+    return network, s, t
+
+
+def densest_subgraph(network, s, t):
+    n = network.num_vertices() - 2
+    m = (network.num_edges() - 2*n) / 2
+    l = 0
+    u = m
+    V = None
+    while u - l >= 1/(n * (n-1)):
+        g = (u + l) / 2
+        update_sink_edge_weights(network, s, t, m, g)
+        cap = network.edge_properties["cap"]
+        residual = push_relabel_max_flow(network, s, t, cap) # O(V^3) = O(n^3)
+        partition = min_st_cut(network, s, cap, residual) # O(V+E) = O(n+m)
+        # Vertices with value True belong to the source side of the cut
+        partition[s] = False # remove source node from source partition for convenient view return
+        view = GraphView(network, vfilt=partition)
+        if view.num_vertices() == 0:
+            u = g
+        else:
+            l = g
+            V = view
+    return V
 
 
 if __name__ == '__main__':
     # 1, 2, 3, 4 fully connected, 5 only connected to 1
-    graph = [(5, 1),
-            (1, 2), (1, 3), (1, 4),
-            (2, 3), (2, 4),
-            (3, 4)]
-    network = undirected_to_flow_network(graph)
+    graph = [
+            ('5', '1'),
+            ('1', '2'), ('1', '3'), ('1', '4'),
+            ('2', '3'), ('2', '4'),
+            ('3', '4')]
+    network, s, t = undirected_to_flow_network(graph)
+    subgraph_view = densest_subgraph(network, s, t)
+    lab = subgraph_view.vertex_properties['labels']
+    sub_vertices = set([lab[v] for v in lab])
+    subgraph_edges = []
+    for e in graph:
+        if e[0] in sub_vertices and e[1] in sub_vertices:
+            subgraph_edges.append(e)
+    subgraph = Graph(directed=False)
+    vertex_labels = subgraph.add_edge_list(subgraph_edges, hashed=True)
+    graph_draw(subgraph, vertex_text=vertex_labels,
+               output="densest_subgraph.png")
