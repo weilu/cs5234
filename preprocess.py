4a684d8b2bc5c31bc9c9afb81e872009d4f394f5
Wei Lu <luwei.here@gmail.com>
2018-11-09 13:10:40 +0800

    Prevent preprocess script from processing already processed data files

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,20 @@
 from glob import glob
 import os.path
 
+PROCESSED_POSTFIX = '_preprocessed.txt'
+
 # turn (a, b) & (b, a) entries to just (a, b)
 def dedupe_undirected_and_remove_comments():
     inputs = glob(os.path.join("data", "*.txt"))
     for filename in inputs:
+        if PROCESSED_POSTFIX in filename:
+            continue
         with open(filename) as f:
             print(f'Prerocessing {filename}')
             lines = f.readlines()
             lines = filter(lambda l: not l.startswith('#'), lines)
-            edges = list(set(map(lambda l: tuple(sorted(l.split())), lines)))
-            outname = filename.replace('.txt', '_preprocessed.txt')
+            edges = sorted(list(set(map(lambda l: tuple(sorted(l.split())), lines))))
+            outname = filename.replace('.txt', PROCESSED_POSTFIX)
             with open(outname, 'w') as o:
                 edge_list = map(lambda pair: '\t'.join(pair), edges)
                 o.write('\n'.join(edge_list))
